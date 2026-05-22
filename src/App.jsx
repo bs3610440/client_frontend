1a0af851ad9e35e983afef5bdf1eb983efb7cc9f
Simran Jeet Kaur <bs3610440@gmail.com>
2026-05-22 14:14:38 +0530

    hii

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./Components/Auth/Signup.jsx";
+import Home from "./Components/Home/Home.jsx";
+import Navbar from "./Components/Navbar/Navbar.jsx";
+import Login from "./Components/Auth/Login.jsx";
+import Dashboard from "./Components/Dashboard/Homedashboard.jsx";
+ import Footer from "./Components/Footer/Footer.jsx";
+import OtpVerification from "./Components/OtpVerification/Otp.jsx";
+import { CartProvider } from "./Pages/CartContext.jsx"
+import {AuthProvider} from "./Components/context/AllContext.jsx"
 
-function App() {
-  const [count, setCount] = useState(0)
+import Newarrival from "./Components/Newarrival.jsx";
+import Collection from "./Components/Collection.jsx";
+import Trending from "./Components/Trending.jsx";
+import Sale from "./Components/Sale.jsx";
 
+
+import { ThemeProvider } from "./Components/context/Themecontext.jsx"
+import SearchResults from "./Components/Search/SearchBar.jsx";
+import Cart from "./Pages/Cart.jsx";
+import Wishlist from "./Pages/Wishlist.jsx";
+
+
+
+export default function App() {
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <ThemeProvider>
+  <AuthProvider>  
+ <CartProvider> 
+    <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
 
-      <div className="ticks"></div>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/otp/user_otp_verification/:id" element={<OtpVerification />} />
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+          <Route path="/new-arrivals" element={<Newarrival/>} />
+           <Route path="/trending" element={<Trending/>} />
+          <Route path="/collections" element={<Collection/>} />
+          <Route path="/sale" element={<Sale/>} />
+           <Route path="/wishlist" element={<Wishlist/>} />
+          <Route path="/cart" element={<Cart/>} />
+         <Route path="/search" element={<SearchResults />} />
+
+
+
+        </Routes>
+      <Footer />
+    </BrowserRouter>
+     </CartProvider>
+      </AuthProvider>
+        </ThemeProvider>
+
+  );
 }
-
-export default App
